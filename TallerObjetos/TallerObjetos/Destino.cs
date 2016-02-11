0b7029848a834f92cdd8ecf3165fb5f760e34e9d
uywilly <william.machado.uy@gmail.com>
2016-02-11 04:30:12 -0800

    Realizados algunos cambios

---
+++ TallerObjetos/TallerObjetos/Destino.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerObjetos
+{
+    public class Destino
+    {
+        private string nombre;
+    }
+}

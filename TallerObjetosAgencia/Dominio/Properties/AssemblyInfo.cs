47b17b5615290a122f84fd2d6f44c0cdcd8dcb00
uywilly <william.machado.uy@gmail.com>
2016-02-13 03:57:12 -0800

    Agregados Dominio y PresentacionWeb

--- TallerObjetosAgencia/Dominio/Properties/AssemblyInfo.cs
+++ TallerObjetosAgencia/Dominio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TallerObjetosAgencia")]
+[assembly: AssemblyTitle("Dominio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TallerObjetosAgencia")]
+[assembly: AssemblyProduct("Dominio")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09bd4d73-619e-4ec9-ae8a-b7d0ab2d31b8")]
+[assembly: Guid("28507cec-64e4-429a-bb21-5b9e21609e19")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

596d81543be3ee1b0bb17eb0260c958d7efdbb3a
uywilly <william.machado.uy@gmail.com>
2016-02-21 11:37:22 -0300

    repositorio destino

---
+++ TallerObjetosAgencia/Dominio/Repositorios/RepositorioDestinos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Entidades;
+
+namespace Dominio.Repositorios
+{
+    class RepositorioDestinos:IRepositorio<Destino>
+    {
+        private IList<Destino> listaDestinos = new List<Destino>();
+
+        public IEnumerable<Destino> List
+        {
+            get { return this.listaDestinos; }
+        }
+
+        public bool Add(Destino entity)
+        {
+            bool retorno = false;
+            return retorno;
+        }
+
+        public bool Delete(Destino entity)
+        {
+            bool retorno = false;
+            return retorno; ;
+        }
+
+        public bool Update(Destino entity)
+        {
+            bool retorno = false;
+            return retorno; 
+        }
+
+        public Destino FindById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
